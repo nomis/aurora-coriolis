df5045426ca93810c17da9ea8bd682949d6aee57
Simon Arlott <sa.me.uk>
2022-11-04 13:02:21 +0000

    Remove use of custom mp_obj_str_modulo_format()

--- src/ulogging.cpp
+++ src/ulogging.cpp
@@ -117,6 +117,8 @@ inline mp_int_t ULogging::to_py_level(Level level) {
 
 mp_obj_t ULogging::do_log(qstr fn, mp_int_t py_level, bool exc_info,
 		size_t n_args, const mp_obj_t *args, mp_map_t *kwargs) {
+	static constexpr size_t STACK_TUPLE_NUM = 8;
+
 	Level level = enabled_level(py_level);
 
 	if (level == Level::OFF)
@@ -134,14 +136,23 @@ mp_obj_t ULogging::do_log(qstr fn, mp_int_t py_level, bool exc_info,
 	if (n_args == 1) {
 		mp_obj_print_helper(print->context(), args[0], PRINT_STR);
 	} else {
-		mp_obj_t dict = MP_OBJ_NULL;
+		mp_obj_t message;
 
-		if (n_args == 2 && mp_obj_is_type(args[1], &mp_type_dict))
-			dict = args[1];
+		if (n_args == 2 && mp_obj_is_type(args[1], &mp_type_dict)) {
+			message = mp_obj_str_binary_op(MP_BINARY_OP_MODULO, args[0], args[1]);
+		} else if (n_args - 1 <= STACK_TUPLE_NUM) {
+			TUPLE_FIXED0(tuple, STACK_TUPLE_NUM);
 
-		mp_obj_print_helper(print->context(),
-			mp_obj_str_modulo_format(args[0], n_args - 1, &args[1], dict),
-			PRINT_STR);
+			tuple.len = n_args - 1;
+			memcpy(&tuple.items[0], &args[1], tuple.len * sizeof(tuple.items[0]));
+
+			message = mp_obj_str_binary_op(MP_BINARY_OP_MODULO, args[0], MP_OBJ_FROM_PTR(&tuple));
+		} else {
+			message = mp_obj_str_binary_op(MP_BINARY_OP_MODULO, args[0],
+				mp_obj_new_tuple(n_args - 1, &args[1]));
+		}
+
+		mp_obj_print_helper(print->context(), message, PRINT_STR);
 	}
 
 	if (parsed_args[ARG_exc_info].u_obj != MP_OBJ_NULL)
