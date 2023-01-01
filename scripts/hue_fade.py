2ed1b95905c42dc36e1ab692b249bdcff16d3173
Simon Arlott <sa.me.uk>
2023-01-01 16:03:00 +0000

    Add hue fade preset

---
+++ scripts/hue_fade.py
@@ -0,0 +1,26 @@
+# aurora-coriolis - ESP32 WS281x multi-channel LED controller with MicroPython
+# Copyright 2023  Simon Arlott
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import aurcor
+
+aurcor.register_config({"duration": ("s32", 21000)})
+config = {}
+
+while True:
+	if aurcor.config(config):
+		config["duration"] = max(1, config["duration"])
+	hue = (aurcor.time_ms() % config["duration"]) / config["duration"]
+	aurcor.output_exp_hsv([hue], repeat=True, fps=60)
